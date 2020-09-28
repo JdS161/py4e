8105a9d621ddf433c40cd953f1e2c0c0bb79fa40
JdS161 <a.vasiltschenko@gmail.com>
2020-09-28 12:16:21 +0300

    XML complete Chapter 13.4 creation

--- Chapter_13.4_(JSON)/ex_13.4_1.py
+++ Chapter_13.4_(JSON)/ex_13.4_1.py
@@ -1,25 +1,24 @@
+import json
 import urllib.request, urllib.parse, urllib.error
-import xml.etree.ElementTree as ET
 import ssl
 
-api_key = False
+
 
 # Ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
+
+
 url = input('Enter - ')
 data = urllib.request.urlopen(url, context=ctx).read()
 print('Retrieved', len(data), 'characters')
 
-decdata = data.decode()
-tree = ET.fromstring(decdata)
-counts = tree.findall('.//count')
-print('Count:', len(counts))
-
-summ =0
-for item in counts:
-    it = int(item.text)
-    summ += it
-print('Sum: ', summ)
+count = 0
+info = json.loads(data)
+for item in info["comments"]:
+	#print item["count"]
+	number = int(item["count"])
+	count = count + number
+print (count)
